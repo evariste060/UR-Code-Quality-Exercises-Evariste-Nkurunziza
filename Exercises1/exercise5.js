9eed72a916cc8e5f7b0cbc6835eb1cd82f69e093
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2026-04-24 19:28:57 +0200

    Change variable name

--- Exercises1/exercise5.js
+++ Exercises1/exercise5.js
@@ -1,4 +1,4 @@
-function calculateFactorial(n) {
-  if (n === 0 || n === 1)  return 1 // Base Case
-  return n*calculateFactorial(n-1) // recursive call
+function calculateFactorial(number) {
+  if (number === 0 || number === 1)  return 1// Base Case
+  return number*calculateFactorial(number-1) // recursive call
 }
