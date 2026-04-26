f02ac4e53a41b102c3d7a80a557534c8894cbc80
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2026-04-26 21:51:06 +0200

    Refactor the prime function

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,7 +1,6 @@
 function isPrime(number) {
   if (number <= 1) return false;   // 0 and 1 are not prime
   if (number === 2) return true;   // 2 is the only even prime
-  if (number % 2 === 0) return false; // eliminate even numbers
   for (let divisor = 2; divisor < number; divisor += 1) {
     if (number % divisor === 0) {
       return false;
