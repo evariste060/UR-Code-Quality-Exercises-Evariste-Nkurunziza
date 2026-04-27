a008f866908d6fa1f3b37f180941c9e9452036e3
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2026-04-27 18:01:30 +0200

    Optimize the prime number function

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -2,10 +2,11 @@ function isPrime(number) {
   if (number <= 1) return false;   // 0 and 1 are not prime
   if (number === 2) return true;   // 2 is the only even prime
   if (number % 2 === 0) return false; // eliminate even numbers
-  for (let divisor = 2; divisor < number; divisor += 1) {
+  for (let divisor = 3; divisor < number; divisor += 2) {
     if (number % divisor === 0) {
       return false;
     }
   }
   return true;
 }
+console.log(isPrime(17))
